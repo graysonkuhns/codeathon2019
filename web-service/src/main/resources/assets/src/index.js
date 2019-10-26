536ac4c6758988c1fdab44d1d555ee91e68f541c
JohnIrle <irlejohn@gmail.com>
2019-10-26 11:28:09 -0500

    Add material-ui css links.

--- web-service/src/main/resources/assets/src/index.js
+++ web-service/src/main/resources/assets/src/index.js
@@ -1,14 +1,6 @@
-import '@babel/polyfill';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-function App() {
-    return (
-        <div>Hello World!</div>
-    )
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-);

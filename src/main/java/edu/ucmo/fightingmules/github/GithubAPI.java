64a4c52167b743392a767d39df7e798a5f5e6277
Jon Bockhorst <jmbockhorst@gmail.com>
2019-10-26 10:03:53 -0500

    Basic request to get repos

--- src/main/java/edu/ucmo/fightingmules/github/GithubAPI.java
+++ src/main/java/edu/ucmo/fightingmules/github/GithubAPI.java
@@ -1,35 +1,50 @@
 package edu.ucmo.fightingmules.github;
 
-import org.eclipse.egit.github.core.*;
-import org.eclipse.egit.github.core.service.IssueService;
-import org.eclipse.egit.github.core.service.RepositoryService;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class GithubAPI {
-    public void getUserContributions(String username){
+    // Method to encode a string value using `UTF-8` encoding scheme
+    private static String encodeValue(String value) {
+        try {
+            return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException ex) {
+            throw new RuntimeException(ex.getCause());
+        }
+    }
 
-        RepositoryService repositoryService = new RepositoryService();
-        IssueService issueService = new IssueService();
+    public void getRepositories(String username){
+        // Get all repos of a user
+        String uri = "https://api.github.com/users/" + username + "/repos?type=all";
+
+        String userAuth = "ucm-codeathon-bot:c8A89Ee6fBTLn-Cp";
+        String basicAuthPayload = "Basic " + Base64.getEncoder().encodeToString(userAuth.getBytes());
 
         try {
-            List<Repository> repos = repositoryService.getRepositories(username);
-            for(Repository r : repos){
-                System.out.println(r.getName() + " - ");
-                List<Contributor> contributors = repositoryService.getContributors(() ->  r.getOwner().getLogin() + "/" + r.getName(), false);
-                List<Issue> issues = issueService.getIssues(username, r.getName(), new HashMap<>());
+            URL url = new URL(uri);
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.addRequestProperty("Authorization", basicAuthPayload);
 
-                for(Contributor c : contributors){
-                    if(c.getLogin().equals(username)){
-                        System.out.println(c.getContributions() + " " + r.getLanguage() + " contributions and " + issues.size() + " issues");
-                    }
-                }
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
             }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
 
+            in.close();
+            connection.disconnect();
+
+            System.out.println(response);
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
